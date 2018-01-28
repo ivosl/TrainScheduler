6a0db1fdf2d92dc8639f8bcf0d5906cb821a55a9
Ivo <ivol@Ivos-MacBook-Air.local>
2018-01-28 00:18:34 -0500

    refinments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,85 +1,91 @@
-/* global moment firebase */
-// Initialize Firebase
-
-var config = {
-    apiKey: "AIzaSyAJaw4TaRdg2E_OOUYtPV3vE0KXOxQ0BXs",
-    authDomain: "train-schedule-f522d.firebaseapp.com",
-    databaseURL: "https://train-schedule-f522d.firebaseio.com",
-    projectId: "train-schedule-f522d",
-    storageBucket: "train-schedule-f522d.appspot.com",
-    messagingSenderId: "1027597058483"
-};
-firebase.initializeApp(config);
-
-// Create a variable to reference the database.
-var database = firebase.database();
-
-// --------------------------------------------------------------
-// Whenever a user clicks the click button
-$("#add-train").on("click", function(event) {
-    event.preventDefault();
+$(document).ready(function() {
     
-    // Get the input values
+    /* global moment firebase */
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAJaw4TaRdg2E_OOUYtPV3vE0KXOxQ0BXs",
+        authDomain: "train-schedule-f522d.firebaseapp.com",
+        databaseURL: "https://train-schedule-f522d.firebaseio.com",
+        projectId: "train-schedule-f522d",
+        storageBucket: "train-schedule-f522d.appspot.com",
+        messagingSenderId: "1027597058483"
+    };
+    firebase.initializeApp(config);
     
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTrain = $("#first-train").val().trim();
-    var frequency = $("#frequency").val().trim();
+    // Create a variable to reference the database.
+    var database = firebase.database();
     
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrain);
-    console.log(frequency);
-    
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-});
-
-database.ref().on("child_added", function(childSnapshot) {
-    
-    var now = moment();
-    firstTrainMoment = moment($("#first-train").val().trim(), "hh:mm");
-    console.log(now);
-    console.log(firstTrainMoment);
-    console.log("difference is ", firstTrainMoment.diff(now, "minutes"), "minutes");
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTrainMoment), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-    
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
-    
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log(nextTrain);
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-    
-    
-    $("#train-list").append(
-        "<div class='row'><div class='col-lg-2'>" 
-        + childSnapshot.val().trainName 
-        + "</div><div class='col-lg-2'>" 
-        + childSnapshot.val().destination 
-        + "</div><div class = 'col-lg-2'>" 
-        + childSnapshot.val().frequency
-        + "</div><div class='col-lg-2'>" 
-        + tMinutesTillTrain
-        + "</div><div class='col-lg-2'>" 
-        + moment(nextTrain).format("hh:mm")
-        + "</div></div>");
+    // --------------------------------------------------------------
+    // Whenever a user clicks the click button
+    $("#add-train").on("click", function(event) {
+        event.preventDefault();
         
-        // Handle the errors
-    }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
+        // Get the input values
+        
+        var trainName = $("#train-name").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrain = $("#first-train").val().trim();
+        var frequency = $("#frequency").val().trim();
+        
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrain);
+        console.log(frequency);
+        
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+        });
+    });
+    
+    database.ref().on("child_added", function(childSnapshot) {
+        
+        // Recording current time and the first train time
+        var now = moment();
+        firstTrainMoment = moment($("#first-train").val().trim(), "hh:mm");
+        console.log(now);
+        console.log(firstTrainMoment);
+        console.log("difference is ", firstTrainMoment.diff(now, "minutes"), "minutes");
+        
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTrainMoment), "minutes");
+        console.log("Difference in time: " + diffTime);
+        
+        // Accessing the frequency value
+        var freq = childSnapshot.val().frequency;
+        console.log(freq);
+        
+        // Time apart (remainder)
+        var remainder = diffTime % freq;
+        console.log(remainder);
+        
+        // Minute Until Train
+        var minutesTillTrain = freq - remainder;
+        console.log("Minites till train: " + minutesTillTrain);
+        
+        // Next Train
+        var nextTrain = moment().add(minutesTillTrain, "minutes");
+        console.log(nextTrain);
+        console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
+        
+        // Appending all the values to the chart
+        $("#train-list").append(
+            "<div class='row'><div class='col-lg-2'>" 
+            + childSnapshot.val().trainName 
+            + "</div><div class='col-lg-2'>" 
+            + childSnapshot.val().destination 
+            + "</div><div class = 'col-lg-2'>" 
+            + freq
+            + "</div><div class='col-lg-2'>" 
+            + minutesTillTrain
+            + "</div><div class='col-lg-2'>" 
+            + moment(nextTrain).format("hh:mm")
+            + "</div></div>");
+            
+            // Handle the errors
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
     });
